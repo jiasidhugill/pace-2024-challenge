5a87152f302fe62dac2f8c8a0323e001b0090d7a
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-10-06 12:01:28 -0500

    DecreaseOzone written

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
 import HomeScreen from './pages/HomeScreen';
 import StartingMenu from './pages/StartingMenu';  // The next scene to transition to
+import IncreaseOzone from './pages/IncreaseOzone';  // The next scene to transition to
+import DecreaseOzone from './pages/DecreaseOzone';  // The next scene to transition to
 import './App.css';  // Import your new CSS for transitions
 
 const App = () => {
     const [currentScene, setCurrentScene] = useState('homeScreen');  // State to track the current scene
-    const [fade, setFade] = useState('fade-in');  // State for fade-in/fade-out transitions
-
+    // const [fade, setFade] = useState('fade-in');  // State for fade-in/fade-out transitions
     const handleSceneChange = () => {
-        setFade('fade-out');  // Trigger fade-out animation
-        setTimeout(() => {
-            setCurrentScene('startingMenu');  // After fade-out, change to the next scene
-            setFade('fade-in');  // Start fade-in animation for new scene
-        }, 500);  // 500ms duration for fade-out before switching scenes
+        setCurrentScene('startingMenu');  // Change to StartingMenu scene
+    };
+
+    const handleStartingScreenChange = () => {
+        setCurrentScene('DecreaseOzone');  // Change to Increase Phytoplankton scene
     };
 
     return (
-        <div className={`app-container ${fade}`}>
+        <div>
             {currentScene === 'homeScreen' && <HomeScreen onChoice={handleSceneChange} />}
-            {currentScene === 'startingMenu' && <StartingMenu />}
+
+            {currentScene === 'startingMenu' && <StartingMenu onChoice={handleStartingScreenChange} />}
+
+            {currentScene === 'IncreaseOzone' && <IncreaseOzone onChoice={handleSceneChange} />}
+
+            {currentScene === 'DecreaseOzone' && <DecreaseOzone onChoice={handleSceneChange} />}
         </div>
     );
 };
+
 
 export default App;
--- frontend/src/pages/IncreaseOzone.js
+++ frontend/src/pages/IncreaseOzone.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles/ozone.css';
 
 const IncreaseOzone = () => {
     return (
-        <div>
+        <div className="ozone-container">
             <h2>Increase Ozone Level</h2>
             <p>Your action has contributed to an increase in ozone levels.</p>
         </div>
